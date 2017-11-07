111d6cee1fbc3780def06a3163774987467703be
Matthew Marksbury <mmarksbury@gmail.com>
2017-11-07 09:41:08 -0800

    Initial commit

---
+++ src/DefaultCookieManager.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ctorx.Core.Mvc.Cookies
+{
+    public class DefaultCookieManager : ICookieManager
+    {
+        private readonly IHttpContextAccessor HttpContextAccessor;
+
+        /// <summary>
+        /// ctor the Mighty
+        /// </summary>
+        public DefaultCookieManager(IHttpContextAccessor httpContextAccessor)
+        {
+            this.HttpContextAccessor = httpContextAccessor;
+        }
+
+        /// <summary>
+        /// Gets a cookie
+        /// </summary>
+        public string Get(string cookieKey)
+        {
+            var context = this.HttpContextAccessor.HttpContext;
+            return context.Request.Cookies[cookieKey];
+        }
+
+        /// <summary>
+        /// Sets a cookie
+        /// </summary>
+        public void Set(string cookieKey, string cookieValue, CookieOptions options = null)
+        {
+            var context = this.HttpContextAccessor.HttpContext;
+            if(options != null)
+            {
+                context.Response.Cookies.Append(cookieKey, cookieValue, options);
+            }
+            else
+            {
+                context.Response.Cookies.Append(cookieKey, cookieValue);
+            }
+        }
+
+        /// <summary>
+        /// Deletes a cookie
+        /// </summary>
+        public void Delete(string cookieKey)
+        {
+            var context = this.HttpContextAccessor.HttpContext;
+            context.Response.Cookies.Delete(cookieKey);
+        }
+    }
+}
---
+++ src/ICookieManager.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ctorx.Core.Mvc.Cookies
+{
+    public interface ICookieManager
+    {
+        /// <summary>
+        /// Gets a cookie
+        /// </summary>
+        string Get(string cookieKey);
+
+        /// <summary>
+        /// Sets a cookie
+        /// </summary>
+        void Set(string cookieKey, string cookieValue, CookieOptions options = null);
+
+        /// <summary>
+        /// Deletes a cookie
+        /// </summary>
+        void Delete(string cookieKey);
+    }
+}
