60402b08ad681afaa68e8e7ac16f58152bb6ff3c
Matthew Marksbury <mmarksbury@gmail.com>
2017-11-07 10:08:51 -0800

    Extensions for service collection

---
+++ src/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ctorx.Core.Mvc.Cookies.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds Cookie Management to the application
+        /// </summary>
+        /// <param name="services">IServiceCollection for the application</param>
+        /// <returns>ServiceCollectin</returns>
+        public static IServiceCollection AddCookieManagement(this IServiceCollection services)
+        {
+            services.AddScoped<ICookieManager, DefaultCookieManager>();
+            return services;
+        }
+    }
+}
